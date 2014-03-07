a287adaaa6f1bf2347905b15807b8b427f697a63
XzoRit <xzorit@hotmail.com>
2014-03-07 17:00:11 +0100

    added test for isUuidInclGuard

--- test_uuid_incl_guard.cpp
+++ test_uuid_incl_guard.cpp
@@ -1,6 +1,7 @@
 #include "utils.hpp"
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+#include <string>
 
 TEST_CASE("search for copyright notice is case insensitive", "hasCopyrightNotice")
 {
@@ -18,4 +19,34 @@ TEST_CASE("has include guard returns false if no guard could be found", "hasIncl
 {
   MaybeInclGuard const guard = hasInclGuard("");
   CHECK_FALSE(guard);
+}
+
+TEST_CASE("is uuid id include guard returns true if guard starts with "
+	  "INCL "
+	  "followed by _ followed by 8 hex digits "
+	  "followed by _ followed by 4 hex digits "
+	  "followed by _ followed by 4 hex digits "
+	  "followed by _ followed by 4 hex digits "
+	  "followed by _ followed by 8 hex digits ", "isUuidInclGuard")
+{
+  SECTION("lower case hex digits")
+    {
+      CHECK(isUuidInclGuard("INCL_01234567_89ab_cdef_ba98_76543210"));
+    }
+  SECTION("upper case hex digits")
+    {
+      CHECK(isUuidInclGuard("INCL_01234567_89AB_CDEF_BA98_76543210"));
+    }
+  SECTION("lower case incl prefix")
+    {
+      CHECK_FALSE(isUuidInclGuard("incl_01234567_89ab_cdef_ba98_76543210"));
+    }
+  SECTION("wrong group seperator")
+    {
+      CHECK_FALSE(isUuidInclGuard("INCL-01234567-89ab-cdef-ba98-76543210"));
+    }
+  SECTION("no incl prefix")
+    {
+      CHECK_FALSE(isUuidInclGuard("01234567_89ab_cdef_ba98_76543210"));
+    }
 }
