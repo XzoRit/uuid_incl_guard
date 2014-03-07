55491fd25421f301f3b4189c57856c194a3150c2
XzoRit <xzorit@hotmail.com>
2014-03-07 16:39:48 +0100

    added test for has copyright and has include guard

--- test_uuid_incl_guard.cpp
+++ test_uuid_incl_guard.cpp
@@ -1,2 +1,21 @@
+#include "utils.hpp"
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+
+TEST_CASE("search for copyright notice is case insensitive", "hasCopyrightNotice")
+{
+  CHECK(hasCopyrightNotice("Copyright"));
+  CHECK(hasCopyrightNotice("copyright"));
+}
+
+TEST_CASE("has include guard returns found guard", "hasInclGuard")
+{
+  MaybeInclGuard const guard = hasInclGuard("#ifndef _MY_TYPICAL_INCL_GUARD_H_");
+  CHECK(guard.get() == "_MY_TYPICAL_INCL_GUARD_H_");
+}
+
+TEST_CASE("has include guard returns false if no guard could be found", "hasInclGuard")
+{
+  MaybeInclGuard const guard = hasInclGuard("");
+  CHECK_FALSE(guard);
+}
